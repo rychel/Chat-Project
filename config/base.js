357c1245b6c11977e0876bbcf261f0d1fdee2ee0
Maxence <maxrychel100@gmail.com>
2022-04-02 07:09:50 +0200

    suppression du fichier versel.js

--- config/base.js
+++ config/base.js
@@ -6,6 +6,7 @@ const connexion = mysql.createConnection({
     password: '',
     database: 'anonymous'
 });
+//postgres://bsbqunpkfzlqju:bedcd1d4dc9b0d8505bb76c3e7afa41afaf1552e309cf1b2c6eb815e1e29727f@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d4m2orlhd1jq6e
 
 connexion.connect((erreur) => {
     if (erreur){
