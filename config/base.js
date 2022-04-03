835c9c1be897113b323aedd4bca7f4734439b3a2
Maxence <maxrychel100@gmail.com>
2022-04-03 02:20:54 +0200

    change main file laucher

--- config/base.js
+++ config/base.js
@@ -12,20 +12,10 @@ connexion.connect((erreur) => {
     if (erreur){
         return console.error('error: ' + erreur.message);
     }
-    //console.log("Connecté à la base de données MySQL!");
-
     connexion.query("CREATE DATABASE IF NOT EXISTS anonymous", function(erreur, resultat) {
         if (erreur) throw erreur;
         console.log("Database created"); 
     });
 });
 
-
-
 module.exports = connexion;
-
-/*
-        connexion.query("CREATE TABLE ID_AGENCE (nom_agence VARCHAR(30) NOT NULL PRIMARY KEY, nom_organisation VARCHAR(30) NOT NULL UNIQUE, nombre_GP INT NOT NULL)", function(erreur, resultat) {
-            if (erreur) throw erreur;
-            console.log("table crée");
-        });*/
