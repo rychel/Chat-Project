1fac4be3f9076c5b37c9c83c4f02b6d3a482ccac
Maxence <maxrychel100@gmail.com>
2022-04-04 01:59:19 +0200

    change config file connection

--- config/base.js
+++ config/base.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const connexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'ec2-52-30-67-143.eu-west-1.compute.amazonaws.com',
+    user: 'dbas9gj434un6f',
+    password: 'ef454694f3ec3d5af64138edd9c0b84103b81ddd203b68f79cae79f9a2f121f8',
     database: 'anonymous'
 });
 //postgres://bsbqunpkfzlqju:bedcd1d4dc9b0d8505bb76c3e7afa41afaf1552e309cf1b2c6eb815e1e29727f@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d4m2orlhd1jq6e
